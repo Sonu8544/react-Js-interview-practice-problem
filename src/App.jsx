1a4ff36c0567d5f0cc5afe267affaec6c3213035
Sonu Kumar <sksonu8544@gmail.com>
2023-10-20 07:41:19 +0530

    Problem First Solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import OneHelloWorld from './components/One/OneHelloWorld'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Lorem, ipsum dolor.</h1>
+      <OneHelloWorld />
     </>
   )
 }
