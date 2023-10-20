0756755c1a9a69169056b7855e0d0135c9e3db45
Sonu Kumar <sksonu8544@gmail.com>
2023-10-20 07:44:30 +0530

    clean App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-
 import OneHelloWorld from './components/One/OneHelloWorld'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
