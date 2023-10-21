3b9e4cc6a766b4bcd69cb56179558e70271ced76
Sonu Kumar <sksonu8544@gmail.com>
2023-10-21 06:21:54 +0530

    Done rander list of item using map

--- src/components/Four/RanderListOfItem.tsx
+++ src/components/Four/RanderListOfItem.tsx
@@ -6,22 +6,22 @@ const RanderListOfItem = () => {
     const listOfItem = ["one", "Two", "Three", "Four", "Five"];
 
 
-  return (
-   <>
-     <ul>
-        <h1>List Item Here UL - LI</h1>
-        { listOfItem.map((item, index) =>( 
-            <li key={index}> {item} </li>
-        )) }
-     </ul>
+    return (
+        <>
+            <ul>
+                <h1>List Item Here UL - LI</h1>
+                {listOfItem.map((item, index) => (
+                    <li key={index}> {item} </li>
+                ))}
+            </ul>
 
 
-   <h1>Item Goes Here</h1>
-   { listOfItem.map((item, index) =>(
-    <h1 key={index} > {item} </h1>
-   )) }
-   </>
-  )
+            <h1>Item Goes Here</h1>
+            {listOfItem.map((item, index) => (
+                <h1 key={index} > {item} </h1>
+            ))}
+        </>
+    )
 }
 
 export default RanderListOfItem
